49148145b34fd6b8ba4e9ff47f0e8ca57cc80466
unaiurkiola <unaiurkiola@gmail.com>
2015-04-15 10:13:25 +0200

    Prueba www

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var http = require("http");
+
+  
 
 app.use(express.static(__dirname + '/public'));
 
@@ -12,3 +15,9 @@ app.get('/', function(req, res) {
 var server = app.listen(process.env.PORT || 3000, function(){
     console.log('Listening in port %d', server.address().port);
 });
+
+
+  http.createServer(function (req, res) {
+    res.writeHead(301, {"Location": "http://www.enigmatic-chamber-1976.herokuapp.com"});
+    res.end();
+}).listen(80);
